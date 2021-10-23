93badd07b08a2aa058ab9f11496bd00233e9977f
emaolv <233103498@qq.com>
2021-10-23 23:32:10 +0800

    实现selectPage

--- service/service_edu/src/main/java/com/mall/emaolv/service/edu/serviceImpl/TeacherServiceImpl.java
+++ service/service_edu/src/main/java/com/mall/emaolv/service/edu/serviceImpl/TeacherServiceImpl.java
@@ -1,10 +1,15 @@
 package com.mall.emaolv.service.edu.serviceImpl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.mall.emaolv.service.edu.entity.Teacher;
+import com.mall.emaolv.service.edu.entity.vo.TeacherQueryVo;
 import com.mall.emaolv.service.edu.mapper.TeacherMapper;
 import com.mall.emaolv.service.edu.service.TeacherService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 /**
  * <p>
@@ -16,5 +21,40 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class TeacherServiceImpl extends ServiceImpl<TeacherMapper, Teacher> implements TeacherService {
+    @Override
+    public IPage<Teacher> selectPage(Long page, Long limit, TeacherQueryVo teacherQueryVo) {
 
+        Page<Teacher> pageParam = new Page<>(page, limit);
+
+        QueryWrapper<Teacher> queryWrapper = new QueryWrapper<>();
+        queryWrapper.orderByAsc("sort");
+
+        if (teacherQueryVo == null){
+            return baseMapper.selectPage(pageParam, queryWrapper);
+        }
+
+        String name = teacherQueryVo.getName();
+        Integer level = teacherQueryVo.getLevel();
+        String begin = teacherQueryVo.getJoinDateBegin();
+        String end = teacherQueryVo.getJoinDateEnd();
+
+        if (!StringUtils.isEmpty(name)) {
+            //左%会使索引失效
+            queryWrapper.likeRight("name", name);
+        }
+
+        if (level != null) {
+            queryWrapper.eq("level", level);
+        }
+
+        if (!StringUtils.isEmpty(begin)) {
+            queryWrapper.ge("join_date", begin);
+        }
+
+        if (!StringUtils.isEmpty(end)) {
+            queryWrapper.le("join_date", end);
+        }
+
+        return baseMapper.selectPage(pageParam, queryWrapper);
+    }
 }
