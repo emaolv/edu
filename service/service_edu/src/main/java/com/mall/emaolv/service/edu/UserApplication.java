db300d5cfde3ee0381811a12ebfe64b834381510
emaolv <233103498@qq.com>
2021-10-23 14:49:15 +0800

    增加ComponentScan 扫描swagger配置信息

--- service/service_edu/src/main/java/com/mall/emaolv/service/edu/UserApplication.java
+++ service/service_edu/src/main/java/com/mall/emaolv/service/edu/UserApplication.java
@@ -1,11 +1,13 @@
 package com.mall.emaolv.service.edu;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
-@MapperScan("com.mall.emaolv.service.edu.mapper")
+@ComponentScan("com.mall.emaolv.service")
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
