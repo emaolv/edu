64402f94cb008e27f26c369e23be3a030d57ccf9
emaolv <233103498@qq.com>
2021-10-23 23:29:05 +0800

    增加GlobalExceptionHandler

---
+++ common/service_base/src/main/java/com/mall/emaolv/service/base/handler/GlobalExceptionHandler.java
@@ -0,0 +1,42 @@
+package com.mall.emaolv.service.base.handler;
+
+import com.mall.emaolv.common.util.R;
+import com.mall.emaolv.common.util.ResultCodeEnum;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.jdbc.BadSqlGrammarException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author ：Xiao Hei
+ * @description：GlobalExceptionHandler
+ * @date ：Created in 2021/10/23 22:54
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public R error(Exception e){
+        e.printStackTrace();
+        return R.error();
+    }
+
+    @ExceptionHandler(BadSqlGrammarException.class)
+    @ResponseBody
+    public R error(BadSqlGrammarException e){
+        e.printStackTrace();
+        return R.setResult(ResultCodeEnum.BAD_SQL_GRAMMAR);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseBody
+    public R error(HttpMessageNotReadableException e){
+        e.printStackTrace();
+        return R.setResult(ResultCodeEnum.JSON_PARSE_ERROR);
+    }
+}
+
+
