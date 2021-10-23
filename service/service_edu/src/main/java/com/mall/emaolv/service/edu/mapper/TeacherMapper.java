73bc58baa41e508f8958040cbd04687cc50085fe
emaolv <233103498@qq.com>
2021-10-23 22:30:29 +0800

    增加讲师Mapper

---
+++ service/service_edu/src/main/java/com/mall/emaolv/service/edu/mapper/TeacherMapper.java
@@ -0,0 +1,18 @@
+package com.mall.emaolv.service.edu.mapper;
+
+import com.mall.emaolv.service.edu.entity.Teacher;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ * 讲师 Mapper 接口
+ * </p>
+ *
+ * @author xiaohei
+ * @since 2021-10-23
+ */
+@Mapper
+public interface TeacherMapper extends BaseMapper<Teacher> {
+
+}
