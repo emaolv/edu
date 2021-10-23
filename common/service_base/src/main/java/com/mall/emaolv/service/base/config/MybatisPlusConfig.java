d9abde83018b0656a44654504d980f61ff185c79
emaolv <233103498@qq.com>
2021-10-23 23:26:16 +0800

    增加MybatisPlusConfig

---
+++ common/service_base/src/main/java/com/mall/emaolv/service/base/config/MybatisPlusConfig.java
@@ -0,0 +1,26 @@
+package com.mall.emaolv.service.base.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author ：Xiao Hei
+ * @description：MybatisPlusConfig
+ * @date ：Created in 2021/10/23 22:19
+ */
+@Configuration
+@MapperScan("com.mall.emaolv.service.*.mapper")
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.H2));
+        return interceptor;
+    }
+
+}
