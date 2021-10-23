6acc29b9251a0566405671db21ba6ee9d76525e3
emaolv <233103498@qq.com>
2021-10-23 22:31:07 +0800

    增加讲师Service

---
+++ service/service_edu/src/main/java/com/mall/emaolv/service/edu/service/TeacherService.java
@@ -0,0 +1,16 @@
+package com.mall.emaolv.service.edu.service;
+
+import com.mall.emaolv.service.edu.entity.Teacher;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 讲师 服务类
+ * </p>
+ *
+ * @author xiaohei
+ * @since 2021-10-23
+ */
+public interface TeacherService extends IService<Teacher> {
+
+}
