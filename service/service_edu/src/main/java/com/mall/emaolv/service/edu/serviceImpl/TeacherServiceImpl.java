e4bef82a08a0e9ed9c614701af5532f7da3dc267
emaolv <233103498@qq.com>
2021-10-23 22:31:55 +0800

    增加讲师Service实现类

---
+++ service/service_edu/src/main/java/com/mall/emaolv/service/edu/serviceImpl/TeacherServiceImpl.java
@@ -0,0 +1,20 @@
+package com.mall.emaolv.service.edu.serviceImpl;
+
+import com.mall.emaolv.service.edu.entity.Teacher;
+import com.mall.emaolv.service.edu.mapper.TeacherMapper;
+import com.mall.emaolv.service.edu.service.TeacherService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 讲师 服务实现类
+ * </p>
+ *
+ * @author xiaohei
+ * @since 2021-10-23
+ */
+@Service
+public class TeacherServiceImpl extends ServiceImpl<TeacherMapper, Teacher> implements TeacherService {
+
+}
