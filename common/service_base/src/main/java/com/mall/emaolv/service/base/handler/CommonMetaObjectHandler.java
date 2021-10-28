27ed565fd32b5b10a74b7d88b28214d7799317c0
Jia <233103498@qq.com>
2021-10-28 20:59:20 +0800

    添加自动填充日期功能

--- common/service_base/src/main/java/com/mall/emaolv/service/base/handler/CommonMetaObjectHandler.java
+++ common/service_base/src/main/java/com/mall/emaolv/service/base/handler/CommonMetaObjectHandler.java
@@ -1,6 +1,7 @@
 package com.mall.emaolv.service.base.handler;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
@@ -14,20 +15,19 @@ import java.util.Date;
  * @description：CommonMetaObjectHandler
  * @date ：Created in 2021/10/23 20:33
  */
+@Slf4j
 @Component
 public class CommonMetaObjectHandler implements MetaObjectHandler {
 
     @Override
     public void insertFill(MetaObject metaObject) {
 
-        this.strictInsertFill(metaObject, "gmtCreate", () -> LocalDateTime.now(), LocalDateTime.class);
-
-
+        this.setFieldValByName("gmtCreate", new Date(), metaObject);
+        this.setFieldValByName("gmtModified", new Date(), metaObject);
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
-        this.strictUpdateFill(metaObject, "gmtModified", () -> LocalDateTime.now(), LocalDateTime.class);
-
+        this.setFieldValByName("gmtModified", new Date(), metaObject);
     }
 }
