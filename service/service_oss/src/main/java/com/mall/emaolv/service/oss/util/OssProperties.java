58099ae9dc2f02436b48f57a9d7bef78f0a37ccc
Jia <233103498@qq.com>
2021-10-29 16:29:45 +0800

    增加ConfigurationProperties注解

--- service/service_oss/src/main/java/com/mall/emaolv/service/oss/util/OssProperties.java
+++ service/service_oss/src/main/java/com/mall/emaolv/service/oss/util/OssProperties.java
@@ -2,10 +2,12 @@ package com.mall.emaolv.service.oss.util;
 
 
 import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Data
 @Component
+@ConfigurationProperties(prefix = "aliyun.oss")
 public class OssProperties {
     private String endpoint;
     private String keyid;
