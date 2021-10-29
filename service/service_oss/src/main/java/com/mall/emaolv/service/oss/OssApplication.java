ad09c0fde1059ffd983b1690b099900c407432e2
Jia <233103498@qq.com>
2021-10-29 16:30:22 +0800

    增加OSS启动类

---
+++ service/service_oss/src/main/java/com/mall/emaolv/service/oss/OssApplication.java
@@ -0,0 +1,16 @@
+package com.mall.emaolv.service.oss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+
+@EnableOpenApi
+@ComponentScan({"com.mall.emaolv.service"})
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)//取消数据源自动配置
+public class OssApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OssApplication.class, args);
+    }
+}
