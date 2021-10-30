c9698059fb294f4e0d06d1b7bd7ca1b3e244f061
Jia <233103498@qq.com>
2021-10-30 12:07:27 +0800

    增加nacos服务发现注解

--- service/service_edu/src/main/java/com/mall/emaolv/service/edu/EduApplication.java
+++ service/service_edu/src/main/java/com/mall/emaolv/service/edu/EduApplication.java
@@ -2,15 +2,17 @@ package com.mall.emaolv.service.edu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.oas.annotations.EnableOpenApi;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 /**
  * @author ：Xiao Hei
  * @description：EduApplication
  * @date ：Created in 2021/10/23 19:33
  */
+@EnableDiscoveryClient
 @EnableOpenApi
 @SpringBootApplication
 @ComponentScan("com.mall.emaolv.service")
