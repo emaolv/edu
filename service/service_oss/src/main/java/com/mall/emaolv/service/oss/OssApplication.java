cd0bba25afdb3fb23bacce299cd59055216a6855
Jia <233103498@qq.com>
2021-10-30 12:09:33 +0800

    增加nacos 服务发现注解

--- service/service_oss/src/main/java/com/mall/emaolv/service/oss/OssApplication.java
+++ service/service_oss/src/main/java/com/mall/emaolv/service/oss/OssApplication.java
@@ -3,9 +3,12 @@ package com.mall.emaolv.service.oss;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 
+
+@EnableDiscoveryClient
 @EnableOpenApi
 @ComponentScan({"com.mall.emaolv.service"})
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)//取消数据源自动配置
