fe2d9e259487ceb2fdc1036d5532ec80d9a32658
Jia <233103498@qq.com>
2021-11-09 14:05:54 +0800

    实现removeAvatarById

--- service/service_edu/src/main/java/com/mall/emaolv/service/edu/serviceImpl/TeacherServiceImpl.java
+++ service/service_edu/src/main/java/com/mall/emaolv/service/edu/serviceImpl/TeacherServiceImpl.java
@@ -3,11 +3,14 @@ package com.mall.emaolv.service.edu.serviceImpl;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.mall.emaolv.common.util.R;
 import com.mall.emaolv.service.edu.entity.Teacher;
 import com.mall.emaolv.service.edu.entity.vo.TeacherQueryVo;
+import com.mall.emaolv.service.edu.feign.OssFileService;
 import com.mall.emaolv.service.edu.mapper.TeacherMapper;
 import com.mall.emaolv.service.edu.service.TeacherService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
@@ -24,6 +27,10 @@ import java.util.Map;
  */
 @Service
 public class TeacherServiceImpl extends ServiceImpl<TeacherMapper, Teacher> implements TeacherService {
+
+    @Autowired
+    private OssFileService ossFileService;
+
     @Override
     public IPage<Teacher> selectPage(Long page, Long limit, TeacherQueryVo teacherQueryVo) {
 
@@ -69,5 +76,19 @@ public class TeacherServiceImpl extends ServiceImpl<TeacherMapper, Teacher> impl
         teacherQueryWrapper.like("name", key );
         List<Map<String, Object>> list = baseMapper.selectMaps(teacherQueryWrapper);
         return list;
+    }
+
+    @Override
+    public boolean removeAvatarById(String id) {
+        Teacher teacher = baseMapper.selectById(id);
+        if(teacher != null){
+            String avatar = teacher.getAvatar();
+            if(!StringUtils.isEmpty(avatar)){
+                System.out.println("ossFileService被调用");
+                R r = ossFileService.removeFile(avatar);
+                return r.getSuccess();
+            }
+        }
+        return false;
     }
 }
