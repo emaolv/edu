a962cbde2b02b8d0785f2f498ab82f5da38fe753
Jia <233103498@qq.com>
2021-11-09 14:06:48 +0800

    增加@EnableFeignClients注解

--- service/service_edu/src/main/java/com/mall/emaolv/service/edu/EduApplication.java
+++ service/service_edu/src/main/java/com/mall/emaolv/service/edu/EduApplication.java
@@ -3,6 +3,7 @@ package com.mall.emaolv.service.edu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 
@@ -12,6 +13,7 @@ import springfox.documentation.oas.annotations.EnableOpenApi;
  * @description：EduApplication
  * @date ：Created in 2021/10/23 19:33
  */
+@EnableFeignClients
 @EnableDiscoveryClient
 @EnableOpenApi
 @SpringBootApplication
