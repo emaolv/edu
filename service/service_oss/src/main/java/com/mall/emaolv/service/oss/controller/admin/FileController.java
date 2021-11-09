182d401d1140f9308382178d42d92177e104a239
Jia <233103498@qq.com>
2021-11-09 14:09:02 +0800

    新增图片删除功能

--- service/service_oss/src/main/java/com/mall/emaolv/service/oss/controller/admin/FileController.java
+++ service/service_oss/src/main/java/com/mall/emaolv/service/oss/controller/admin/FileController.java
@@ -4,19 +4,25 @@ import com.mall.emaolv.common.util.R;
 import com.mall.emaolv.common.util.ResultCodeEnum;
 import com.mall.emaolv.service.base.handler.EduException;
 import com.mall.emaolv.service.oss.service.FileService;
+import com.oracle.tools.packager.Log;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
 @Api(tags="阿里文件管理")
 @CrossOrigin
 @RestController
-@RequestMapping(value = "/admin/oss/file", method = {RequestMethod.POST}, headers = "content-type=multipart/form-data")
+// headers = "content-type=multipart/form-data
+@RequestMapping(value = "/admin/oss/file")
 public class FileController {
 
     @Autowired
@@ -47,4 +53,14 @@ public class FileController {
            throw new EduException(ResultCodeEnum.BAD_SQL_GRAMMAR);
        }
     }
+
+    @ApiOperation(value="文件删除")
+    @DeleteMapping("remove")
+    public R removeFile(
+            @ApiParam(value = "要删除的文件", required = true)
+            @RequestBody String url){
+        fileService.removeFile(url);
+        return R.ok().message("文件删除成功");
+    }
+
 }
